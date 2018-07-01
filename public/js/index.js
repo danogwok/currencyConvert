0aba4e492b48c3b433917de83acfad81585821b0
Daniel Ogwok <danogwok@yahoo.co.uk>
2018-07-01 15:14:03 +0200

    service worker registration

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,10 @@ $(document).ready(function (){
 	fetchCurrencies();
 });
 
+//Warnings and Errors
+if (!window.indexedDB) {
+    console.log("Your browser doesn't support a stable version of IndexedDB");
+}
 
 //fetch fetch Currencies
 function fetchCurrencies()
@@ -33,15 +37,15 @@ function convertCurrencies()
     let from 	= $("#from").val();
     let to 		= $("#to").val();
 
+    //Clear the alert incase still existant
+    $(".alert").html("");
     //Make sure user is not converting the same currency
     if(from == to)
     {
       $(".alert").html(`
-  			<div class="card-feel">
   				<span class="text-danger">
-  					Ops!, you can't convert the same currency
+  					Can't convert same currency
   				</span>
-  			</div>
   		`);
     }
 
@@ -60,30 +64,197 @@ function convertCurrencies()
       //console.log(currencies_list);
       //loop through each of the currency arrays/objects then add the to drop down options
       $.each(currencies_list, function(index, val) {
-        console.log(val);
-        $(".result").append("Hi");
-        // $(".result").append (`
-        //   <div class="card-feel">
-        //     <b>${numeral(amount * val.val).format('0.000')} in ${val.to}</b>
-        //   </div>
-        // `);
+        console.log(val.val * amount);
+        let total_amount = val.to + " " + (val.val * amount).toFixed(2);
+      //  $(".result").append("Hi");
+        //Clear div from DOM
+        $(".result").html("");
+        //Append to Div
+        $(".result").append (`
+          <div class="card-feel">
+            <b>${total_amount}</b>
+          </div>
+        `);
 
         //Create data Object that will be used later
-        let object = {
+        let   currency = {
   				symbol: body,
   				value: val.val
   			};
 
         //Store the object to the openDatabase
+        saveDB(currency);
 
     })
     .catch(function(err) {
       console.log("status: ", err.status);
-      // fecth currency from indexDB
-      //fetchFromDatabase(body, amount);
+        // fecth currency from indexDB
+        getFromDB(body, amount);
     });
   });
+
+  return false;
 }
+
+/**
+* First create a database and then open it
+*/
+//Save to //
+function saveDB(value)
+{
+    //instance variables
+    const DATA_BASE = "myDB";
+    const db_access = IndexedDB.open(DATA_BASE, 1);
+
+    //Catch any errors that might occur
+    db_access.onerror = (event) => {console.log("database has an error"); return false; };
+
+    //Monitor changes and the versions
+    db_access.onupgradeneeded = function(event) {
+        // listen for the event response
+        var upgradeDB = event.target.result;
+        // create an objectStore for this database
+        var objectStore = upgradeDB.createObjectStore("currencies");
+    };
+
+    // on success add user
+  	db_access.onsuccess = (event) => {
+
+  		// console.log('database has been openned !');
+  		const query = event.target.result;
+
+  	  	// check if already exist symbol
+  		const currency = query.transaction("currencies").objectStore("currencies").get(data.symbol);
+
+  		// wait for users to arrive
+  	  	currency.onsuccess = (event) => {
+  	  		const dbData = event.target.result;
+  	  		const store  = query.transaction("currencies", "readwrite").objectStore("currencies");
+
+  	  		if(!dbData){
+  	  			// save data into currency object
+  				store.add(data, data.symbol);
+  	  		}else{
+  	  			// update data existing currency object
+  				store.put(data, data.symbol);
+  	  		};
+  	  	}
+  	}
+}
+
+//get from the DATA_BASE
+function getFromDB(symbol, amount)
+{
+    // init database
+    const db = openDatabase();
+
+    // on success add user
+    db.onsuccess = (event) => {
+
+      // console.log('database has been openned !');
+      const query = event.target.result;
+
+      // check if already exist symbol
+      const currency = query.transaction("currencies").objectStore("currencies").get(symbol);
+
+      // wait for users to arrive
+        currency.onsuccess = (event) => {
+          const data = event.target.result;
+          // console.log(data);
+          if(data == null){
+            $(".error_msg").append(`
+            <div class="card-feel">
+                      <span class="text-danger">
+                        You are currently offline... check internet connectivity and try again.
+                      </span>
+            </div>
+          `);
+
+          // hide error message
+          setTimeout((e) => {
+            $(".error_msg").html("");
+          }, 1000 * 3);
+
+          // void
+          return;
+          }
+
+        // console.log(data);
+        // console.log(data);
+        let pairs = symbol.split('_');
+        let fr = pairs[0];
+        let to = pairs[1];
+
+        $(".results").append(`
+          <div class="card-feel">
+
+            <hr />
+          <b>${amount}</b> <b>${fr}</b> to <b>${to}</b> is:
+            <b>${numeral(amount * data.value).format('0.000')}</b>
+          </div>
+        `);
+        }
+    }
+}
+
+//Service Workers
+
+// Register Service Worker
+if(navigator.serviceWorker){
+	// register the services worker
+	registerServiceWorker();
+
+	// listen for controller change
+	navigator.serviceWorker.addEventListener('controllerchange', function (){
+		window.location.reload();
+	});
+
+}else{
+	console.log('browser does not support Services Worker !');
+}
+
+// register sw
+function registerServiceWorker() {
+	// register the service worker
+	navigator.serviceWorker.register('js/sw/index.js'/*, {scope: './public/'}*/).then(function(sw) {
+		// check service worker controller
+		if(!navigator.serviceWorker.controller) return;
+
+		// on waiting state
+		if(sw.waiting){
+			// updateIsReady(sw.waiting);
+			sw.postMessage('message', {action: 'skipWaiting'});
+			return;
+		}
+
+		// on installing state
+		if(sw.installing){
+			trackInstalling(sw.installing);
+		}
+
+		// on updated found
+		sw.addEventListener('updatefound', function (){
+			trackInstalling(sw.installing);
+		});
+	});
+}
+
+// track sw state
+function trackInstalling(worker) {
+	worker.addEventListener('statechange', function(){
+		if(worker.state == 'installed'){
+			updateIsReady(worker);
+		}
+	});
+}
+
+// update app
+function updateIsReady(sw){
+	// console.log('a new SW is ready to take over !');
+	// sw.postMessage('message', {action: 'skipWaiting'});
+	pushUpdateFound();
+}
+
 
 // refresh page
 function refreshPage() {
---
+++ public/js/sw/index.js
@@ -0,0 +1,62 @@
+require('serviceworker-cache-polyfill');
+
+var version  = 'v15';
+var staticCacheName = "dbCache";
+
+	//Install event to prepare service worker
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache){
+			return cache.addAll([
+				'./',
+				'css/bootstrap.min.css',
+				'css/style.css',
+				'js/bootstrap.min.js',
+				'js/jquery.min.js'
+			]);
+		})
+	);
+});
+
+//Data to be stored
+var expectedCaches = [
+	staticCacheName,
+	'currency-names',	//Names of the various currencies_list
+	'currency-rate'		//Rates between the different currencies
+];
+
+// on activate state
+self.addEventListener('activate', function(event){
+	var cacheWhitelist = ['staticCacheName'];
+	event.waitUntil(
+		caches.keys().then(function(cacheNames){
+			return Promise.all(
+				cacheNames.map(function(cacheName){
+					IF(cacheWhitelist.indexOf(cacheName) == -1)
+					{
+							return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+});
+
+
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+		caches.match(event.request).then(function(response){
+			if(response){
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
+});
+
+// on message
+self.addEventListener('message', function(event){
+	if(event.data.action == 'skipWaiting'){
+		self.skipWaiting();
+	}
+});
